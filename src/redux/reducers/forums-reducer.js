87911d09c44024fc08526091544e579d66495920
Gustavo loera <gustavoa.loera01@gmail.com>
2021-03-20 00:55:34 -0700

    add forums reducer placeholder

---
+++ src/redux/reducers/forums-reducer.js
@@ -0,0 +1,14 @@
+import { ADD_FORUM, LOAD_FORUMS } from '../action-types/forums-action-types';
+const initialState = [];
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case LOAD_FORUMS:
+      return action.payload;
+    case ADD_FORUM:
+      return [...state, action.payload];
+    // rest of actions...
+    default:
+      return state;
+  }
+};
