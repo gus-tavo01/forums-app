6b20ff38a58be03171be25ffb8daa8e081b3bbd9
Gustavo loera <gustavoa.loera01@gmail.com>
2021-03-20 00:49:36 -0700

    add forums entity action-types

---
+++ src/redux/action-types/forums-action-types.js
@@ -0,0 +1,5 @@
+export const ADD_FORUM = 'ADD_FORUM';
+export const LOAD_FORUMS = 'LOAD_FORUMS';
+export const MODIFY_FORUM = 'MODIFY_FORUM';
+export const REMOVE_FORUM = 'REMOVE_FORUM';
+// get single forum??
