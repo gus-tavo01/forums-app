1794fd41f963862ead33eec1dc418405c6dcf8dc
Gustavo loera <gustavoa.loera01@gmail.com>
2021-03-22 23:23:43 -0700

    add forums service test file

---
+++ src/tests/unit/services/forums-service.test.js
@@ -0,0 +1,22 @@
+import ForumsService from '../../../services/forums-service';
+
+let forumsService;
+
+beforeAll(() => {
+  forumsService = new ForumsService();
+});
+
+describe('Forums Service getByFilters', () => {
+  test('When parameters are valid, expect a list of forums in response', async () => {
+    // Arrange
+    const filters = {
+      page: 1,
+    };
+
+    // Act
+    const response = await forumsService.getForums(filters);
+
+    // Assert
+    expect(response).not.toBeNull();
+  });
+});
