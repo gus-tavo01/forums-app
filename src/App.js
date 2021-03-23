acd380ad491edad749c24108b7e73075b61d4f66
Gustavo loera <gustavoa.loera01@gmail.com>
2021-03-22 23:45:44 -0700

    add primary routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import AppMenuBar from './components/AppMenuBar';
+// pages
+import Login from './pages/Login';
 import Forums from './pages/Forums';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
-    <main>
+    <Router>
       <AppMenuBar />
-      <Forums />
-    </main>
+      <Switch>
+        <Route path="/" exact>
+          <Forums />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/signup">
+          <SignUp />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        {/*
+        /my-forums
+        /notifications
+        /account
+        */}
+        <Route path="*">
+          <NotFound />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
