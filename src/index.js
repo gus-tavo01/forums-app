42957b1748408e2557465e5b42faa4e315edf6e3
Gustavo loera <gustavoa.loera01@gmail.com>
2021-03-24 00:14:29 -0700

    implement theme

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { defaultTheme } from './styles/appTheme';
+import { MuiThemeProvider } from '@material-ui/core/styles';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './redux/store';
@@ -8,7 +10,9 @@ import store from './redux/store';
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
-      <App />
+      <MuiThemeProvider theme={defaultTheme}>
+        <App />
+      </MuiThemeProvider>
     </React.StrictMode>
   </Provider>,
   document.getElementById('root')
