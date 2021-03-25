ba9c86502b752c5ae29b5c3adcd9eaa62246d155
Gustavo loera <gustavoa.loera01@gmail.com>
2021-03-24 22:38:15 -0700

    add notFound page placeholder

---
+++ src/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NotFound() {
+  return (
+    <div>
+      <div>Not Found</div>
+      The content you are looking for does not exist. page is not found
+    </div>
+  );
+}
+
+export default NotFound;
