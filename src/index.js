55232a181bb7bc29410d2cde2c0c0c0e74561e8e
Gustavo loera <gustavoa.loera01@gmail.com>
2021-04-02 01:02:35 -0700

    add baseLine for supporting dark mode

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { defaultTheme } from './styles/appTheme';
 import { MuiThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './redux/store';
@@ -11,6 +12,7 @@ ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
       <MuiThemeProvider theme={defaultTheme}>
+        <CssBaseline />
         <App />
       </MuiThemeProvider>
     </React.StrictMode>
