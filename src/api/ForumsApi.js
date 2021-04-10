efc6cb817ef0e764fd640670306cad7a58f7a66a
Gustavo loera <gustavoa.loera01@gmail.com>
2021-04-09 23:03:20 -0700

    add forums api class

---
+++ src/api/ForumsApi.js
@@ -0,0 +1,50 @@
+import axios from 'axios';
+import { ForumsApiUrl } from '../constants/Api';
+
+function successCallback(response) {
+  return response.data;
+}
+
+function errorCallback(error) {
+  // TODO:
+  // define flow on error
+  const payload = {
+    isSuccess: false,
+    errorMessage: error.message,
+    statusCode: 500,
+  };
+
+  if (error.response) {
+    payload.errorMessage = error.response.data.status_message;
+    payload.statusCode = error.response.status;
+  } else if (error.request) {
+    payload.errorMessage = error.request.data.status_message;
+    payload.statusCode = error.request.status;
+  }
+
+  throw payload;
+}
+
+export default class ForumsApi {
+  constructor(timeout = 35000) {
+    this.timeout = timeout;
+    const instance = axios.create({
+      baseURL: `${ForumsApiUrl}/posts`,
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        // TBD:
+        // Authorization: `Bearer ${token}`,
+      },
+    });
+    instance.interceptors.response.use(successCallback, errorCallback);
+    this.instance = instance;
+  }
+
+  getByFilters = async (params) => this.instance.get('/', params);
+
+  // post
+  // put
+  // delete
+  // getById?
+}
