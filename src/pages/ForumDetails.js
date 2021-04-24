4d4ce26260b87e133301303f5667a39c59445fcb
Gustavo loera <gustavoa.loera01@gmail.com>
2021-04-24 15:24:01 -0700

    update layout and styles, add dummy useEffect

--- src/pages/ForumDetails.js
+++ src/pages/ForumDetails.js
@@ -1,28 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 // mui components
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
 import Avatar from '@material-ui/core/Avatar';
 import Divider from '@material-ui/core/Divider';
 import Typography from '@material-ui/core/Typography';
-import PageTitle from '../components/PageTitle';
 // components
+import FromDate from '../components/FromDate';
+import TopicsList from '../components/TopicsList';
 
 const useStyles = makeStyles((theme) => ({
-  paper: {
-    minWidth: 320,
-    marginTop: 15,
-    padding: 15,
+  root: {
+    minWidth: 300,
+    padding: theme.spacing(1),
   },
   image: {
     width: theme.spacing(8),
     height: theme.spacing(8),
-  },
-  tabs: {
-    margin: '10px 0px',
+    marginRight: theme.spacing(1),
   },
 }));
 
@@ -30,28 +27,40 @@ function ForumDetails() {
   const classes = useStyles();
   const { id } = useParams();
   const forum = useSelector((store) => store.forums.docs.find((f) => f.id === id));
+  const topics = [];
+
+  useEffect(() => {
+    // eslint-disable-next-line no-console
+    console.log('- fetch topics');
+  }, []);
 
   return (
-    <Grid container justify="center">
-      <Paper className={classes.paper}>
-        <Grid container item direction="column">
-          <PageTitle content={forum.name} />
-          <Grid container item justify="center">
-            <Avatar src={null} className={classes.image} />
-            {/* edit button */}
-          </Grid>
-          <Grid container item justify="space-between">
-            <Typography>{forum.author}</Typography>
-            <Typography>Created on {forum.createDate}</Typography>
-          </Grid>
-          <Divider />
-          <Grid item>
-            <Typography>{forum.description}</Typography>
-          </Grid>
-          <Divider />
-          <Grid item>Topics content</Grid>
-        </Grid>
-      </Paper>
+    <Grid container justify="center" direction="column" className={classes.root} spacing={3}>
+      <Grid container item justify="center" alignItems="center">
+        <Avatar src={forum.imageSrc} className={classes.image} />
+        <Typography component="h2" variant="h4">
+          {forum.name}
+        </Typography>
+      </Grid>
+      <Grid container item justify="space-between">
+        <Typography>By: {forum.author}</Typography>
+        <Typography>
+          Created <FromDate>{forum.createDate}</FromDate>
+        </Typography>
+      </Grid>
+      <Divider />
+      <Grid item>
+        <Typography>{forum.description}</Typography>
+      </Grid>
+      <Divider />
+      <Grid item container justify="center">
+        <Typography component="h3" variant="h5">
+          Topics
+        </Typography>
+      </Grid>
+      <Grid container item justify="center" spacing={2}>
+        <TopicsList items={topics} />
+      </Grid>
     </Grid>
   );
 }
