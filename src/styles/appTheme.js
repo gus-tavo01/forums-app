340c3c35debf9913d6462a5c43d4ec2ba16109fd
Gustavo loera <gustavoa.loera01@gmail.com>
2021-04-23 18:25:20 -0700

    update theme eslint issues

--- src/styles/appTheme.js
+++ src/styles/appTheme.js
@@ -1,11 +1,11 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 import { green, grey } from '@material-ui/core/colors';
 
-export const defaultTheme = createMuiTheme({
+export default createMuiTheme({
   palette: {
     type: 'dark',
     primary: {
-      main: green['A400'],
+      main: green.A400,
     },
     secondary: {
       main: grey[50],
