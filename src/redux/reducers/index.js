a3689a9621ca9b9f55a432cbe53555d7799aee05
Gustavo loera <gustavoa.loera01@gmail.com>
2021-04-24 17:19:01 -0700

    add loaders reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
+import loaders from './loaders-reducer';
 import forums from './forums-reducer';
 // posts, messages, errors, loadingStatus
 
-export default combineReducers({ forums });
+export default combineReducers({ forums, loaders });
