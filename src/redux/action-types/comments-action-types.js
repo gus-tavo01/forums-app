6a2a3d94cd7b6cc7935e75b74292ca4f8b357cb1
Gustavo loera <gustavoa.loera01@gmail.com>
2021-05-01 10:40:11 -0700

    add comment action types

---
+++ src/redux/action-types/comments-action-types.js
@@ -0,0 +1,4 @@
+export const ADD_COMMENT = 'ADD_COMMENT';
+export const LOAD_COMMENTS = 'LOAD_COMMENT';
+export const MODIFY_COMMENT = 'MODIFY_COMMENT';
+export const REMOVE_COMMENT = 'REMOVE_COMMENT';
