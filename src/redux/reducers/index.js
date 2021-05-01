33215e773546a8e60111050405cba551f6d6a87f
Gustavo loera <gustavoa.loera01@gmail.com>
2021-05-01 15:58:18 -0700

    init comment reducer on store

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux';
 import loaders from './loaders-reducer';
 import forums from './forums-reducer';
 import topics from './topics-reducer';
-// messages, errors
+import comments from './comments-reducer';
+// errors
 
-export default combineReducers({ forums, loaders, topics });
+export default combineReducers({ forums, loaders, topics, comments });
