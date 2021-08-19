057c421b08042a7369ba0069d22097d5fe328d07
gus-tavo01 <gustavoa.loera01@gmail.com>
2021-08-18 18:43:48 -0700

    quick update on auth action types

---
+++ src/redux/action-types/auth-action-types.js
@@ -0,0 +1,8 @@
+const authConstants = {
+  LOGIN_REQUEST: 'AUTH_LOGIN_REQUEST',
+  LOGIN_SUCCESS: 'AUTH_LOGIN_SUCCESS',
+  LOGIN_FAILURE: 'AUTH_LOGIN_FAILURE',
+  LOGOUT: 'AUTH_LOGOUT',
+};
+
+export default authConstants;
