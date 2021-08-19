41f4411d780e657fcd0c6d1a1df78ae106d111c8
gus-tavo01 <gustavoa.loera01@gmail.com>
2021-08-18 18:46:30 -0700

    add auth reducer file

---
+++ src/redux/reducers/auth-reducer.js
@@ -0,0 +1,26 @@
+import authConstants from '../action-types/auth-action-types';
+
+const { LOGIN_FAILURE, LOGIN_REQUEST, LOGIN_SUCCESS, LOGOUT } = authConstants;
+
+const token = localStorage.getItem('user');
+const clearedState = {};
+const initialState = token ? { isLoggedIn: true, token } : clearedState;
+
+export default (state = initialState, action) => {
+  switch (action) {
+    case LOGIN_REQUEST:
+      return { logginIn: true };
+
+    case LOGIN_SUCCESS:
+      return { isLoggedIn: true, token: action.payload.token };
+
+    case LOGOUT:
+      return clearedState;
+
+    case LOGIN_FAILURE:
+      return clearedState;
+
+    default:
+      return state;
+  }
+};
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import auth from './auth-reducer';
 import loaders from './loaders-reducer';
 import forums from './forums-reducer';
 import topics from './topics-reducer';
 // messages, errors
 
-export default combineReducers({ forums, loaders, topics });
+export default combineReducers({ auth, forums, loaders, topics });
