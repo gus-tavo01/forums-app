bacd2b156a8bd9711a8623f47c5a1cf38ef579cc
Gustavo Loera <gustavoa.loera01@gmail.com>
2021-08-22 10:06:08 -0700

    move action getProfile to auth actions

--- src/redux/actions/auth-actions.js
+++ src/redux/actions/auth-actions.js
@@ -1,7 +1,9 @@
 import AuthService from '../../services/auth-service';
+import UsersService from '../../services/users-service';
 import authConstants from '../action-types/auth-action-types';
 
 const authService = new AuthService();
+const usersService = new UsersService();
 
 export const login = (authData) => async (dispatch) => {
   // Step set loading	state
@@ -27,6 +29,26 @@ export const login = (authData) => async (dispatch) => {
 export const logout = () => {
   sessionStorage.removeItem('user');
   return { type: authConstants.LOGOUT };
+};
+
+export const getProfile = (username, token) => async (dispatch) => {
+  // Step set loading state
+  dispatch({ type: authConstants.GETPROFILE_REQUEST });
+
+  // Step get user from BE
+  const response = usersService.getByLoginName(username, token);
+
+  // Step handle response
+  if (response.errorMessage) {
+    // Step set error state fetch user
+    dispatch({ type: authConstants.GETPROFILE_FAILURE, payload: response.errorMessage });
+    // dispatch alert error
+    return false;
+  }
+
+  // Step set successful fetch request
+  dispatch({ type: authConstants.GETPROFILE_SUCCESS, payload: response.payload });
+  return true;
 };
 
 // register
--- src/redux/actions/users-actions.js
+++ src/redux/actions/users-actions.js
@@ -3,26 +3,6 @@ import usersConstants from '../action-types/users-action-types';
 
 const usersService = new UsersService();
 
-export const fetchProfile = (username, token) => async (dispatch) => {
-  // Step set loading state
-  dispatch({ type: usersConstants.GETPROFILE_REQUEST });
-
-  // Step get user from BE
-  const response = usersService.getByLoginName(username, token);
-
-  // Step handle response
-  if (response.errorMessage) {
-    // Step set error state fetch user
-    dispatch({ type: usersConstants.GETPROFILE_FAILURE, payload: response.errorMessage });
-    // dispatch alert error
-    return false;
-  }
-
-  // Step set successful fetch request
-  dispatch({ type: usersConstants.GETPROFILE_SUCCESS, payload: response.payload });
-  return true;
-};
-
 export const fetchUser = (userId) => async (dispatch) => {
   // Step set loading state
   dispatch({ type: usersConstants.GETBYID_REQUEST });
