915992dbbe3deb3a6c662eecaf0444ddca382dd7
Gustavo Loera <gustavoa.loera01@gmail.com>
2021-08-22 19:46:09 -0700

    resolve theme creator

--- src/styles/appTheme.js
+++ src/styles/appTheme.js
@@ -1,7 +1,7 @@
-import { createMuiTheme } from '@material-ui/core/styles';
+import { createTheme } from '@material-ui/core/styles';
 import { green, grey } from '@material-ui/core/colors';
 
-export default createMuiTheme({
+export default createTheme({
   palette: {
     type: 'dark',
     primary: {
