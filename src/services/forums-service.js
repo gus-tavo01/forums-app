f2a84b5dbca8f448477888ab51fdfefedb89b6ec
Gustavo Loera <gustavoa.loera01@gmail.com>
2021-08-24 18:12:29 -0700

    map param in filters correctly

--- src/services/forums-service.js
+++ src/services/forums-service.js
@@ -7,22 +7,16 @@ export default class ForumsService {
 
   get = async (filters) => {
     const params = {};
-    if (filters.page) {
-      params.page = filters.page;
-    }
-    if (filters.name) {
-      params.name = filters.name;
-    }
-    if (filters.author) {
-      params.author = filters.author;
-    }
-    if (filters.size && filters.size !== 'open') {
-      params.size = filters.size;
-    }
 
-    // TODO
-    // handle errors
-    const response = await this.forumsApi.forumsGet(params);
-    return response;
+    if (filters.page) params.page = filters.page;
+    if (filters.pageSize) params.pageSize = filters.pageSize;
+    if (filters.topic) params.topic = filters.topic;
+    if (filters.author) params.author = filters.author;
+    if (filters.public) params.public = filters.public;
+    if (filters.isActive) params.isActive = filters.isActive;
+    if (filters.sortBy) params.sortBy = filters.sortBy;
+    if (filters.sortOrder) params.sortOrder = filters.sortOrder;
+
+    return this.forumsApi.forumsGet(params);
   };
 }
