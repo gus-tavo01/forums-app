0bc6a95a6909fc3e9b7bf458a6e3c9508df360a2
Gustavo Loera <gustavoa.loera01@gmail.com>
2021-08-28 22:29:22 -0700

    add participants component

---
+++ src/pages/Participants.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// components
+import AppContainer from '../components/AppContainer';
+import PageTitle from '../components/PageTitle';
+
+function Participants() {
+  return (
+    <AppContainer>
+      <PageTitle content="Participants" />
+    </AppContainer>
+  );
+}
+
+export default Participants;
