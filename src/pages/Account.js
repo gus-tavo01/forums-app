861d4263a991c70a072148edd954edd9af9c9d48
Gustavo Loera <gustavoa.loera01@gmail.com>
2021-08-28 22:29:08 -0700

    add My Account component

---
+++ src/pages/Account.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// components
+import AppContainer from '../components/AppContainer';
+import PageTitle from '../components/PageTitle';
+
+function Account() {
+  return (
+    <AppContainer>
+      <div>
+        <PageTitle content="My Account" />
+
+        <p>This page is under construction...</p>
+      </div>
+    </AppContainer>
+  );
+}
+
+export default Account;
