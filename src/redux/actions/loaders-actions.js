8eace84ad40b505f48564aa2c55c88838a54cd26
Gustavo Loera <gustavoa.loera01@gmail.com>
2021-08-29 17:15:25 -0700

    remove topics/loaders actions/reducers

--- src/redux/actions/loaders-actions.js
+++
@@ -1,5 +0,0 @@
-import { SET_FORUMS_IS_LOADING, SET_FORUM_IS_LOADING } from '../action-types/loaders-action-types';
-
-export const setForumsIsLoading = (payload) => ({ payload, action: SET_FORUMS_IS_LOADING });
-
-export const setForumIsLoading = (payload) => ({ payload, action: SET_FORUM_IS_LOADING });
